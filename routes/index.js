4a371f65506bb22c0da5118be9b7f0879f5df8a9
Luna Chouchou <liu2shu1@outlook.com>
2024-06-23 05:27:14 +0900

    案例优化_03_插入数据库

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,14 @@ const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync(__dirname + '/../data/db.json')
 //获取db对象
 const db = low(adapter)
-const shortid = require('shortid')
+//导入shortid
+const shortid = require('shortid');
+//导入 moment
+const moment = require('moment');
+const AccountModel = require('../models/AccountModel');
+
+//测试
+// console.log(moment('2024-06-23').toDate())
 
 //记账本的列表
 router.get('/account', function(req, res, next) {
@@ -21,12 +28,25 @@ router.get('/account/create', function(req, res, next) {
 
 //新增记录
 router.post('/account', (req, res) => {
-  //生成id
-  let id = shortid.generate();
-  //写入文件
-  db.get('accounts').unshift({id:id, ...req.body}).write();
-  //成功提醒
-  res.render('success', {msg: '添加成功哦~~~', url: '/account'});
+  //查看表单数据 2024-06-23 => new Date()
+  // 2024-06-23 => moment => new Date()
+  // console.log(req.body);
+  //修改req.body.time的值
+  // console.log(req.body.time); //'2024-06-23'
+  // req.body.time = moment(req.body.time).toDate()
+  //插入数据库
+  AccountModel.create({
+    ...req.body,
+    //修改 time 属性的值
+    time: moment(req.body.time).toDate()
+  }).then(() => {
+    //成功提醒
+    res.render('success', {msg: '添加成功哦~~~', url: '/account'});
+  }).catch((err) => {
+    res.status(500).send('插入失败~~~');
+    console.log(err);
+    return;
+  })
 })
 
 //删除记录
