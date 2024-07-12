8415497b92b3b7c05fd45faadae41f8878f1aa6d
Luna Chouchou <liu2shu1@outlook.com>
2024-07-13 01:59:29 +0900

    案例功能完善_03_用户登录

--- routes/web/auth.js
+++ routes/web/auth.js
@@ -21,4 +21,31 @@ router.post('/reg', (req, res) => {
     return
   });
 })
+
+//登录页面
+router.get('/login', (req, res) => {
+    //响应html内容
+    res.render('auth/login');
+})
+
+//登录操作
+router.post('/login', (req, res) => {
+  //获取用户名和密码
+  let { username, password } = req.body;
+  //查询数据库
+  UserModel.findOne({username: username, password: md5(password)}).then((data) => {
+    console.log(data);
+    //判断的ata
+    if(!data){
+        return res.send('账号或密码错误~~');
+    }
+    //登录成功响应
+    res.render('success', {msg: '登录成功', url: '/account'});
+  }).catch((err) => {
+    res.status(500).send('登录失败, 请稍后再试');
+    return
+  });
+})
+
+
 module.exports = router;
